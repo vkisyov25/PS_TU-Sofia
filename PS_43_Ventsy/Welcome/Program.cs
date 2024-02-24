464e1963f6013135c9a1820993b4f3cfd300cd80
Ventsislav Kisyov <ventsislav755@gmail.com>
2024-02-24 12:12:48 +0200

    added solution on exexrcise one

---
+++ PS_43_Ventsy/Welcome/Program.cs
@@ -0,0 +1,24 @@
+﻿using Welcome.Model;
+using Welcome.Others;
+using Welcome.View;
+using Welcome.ViewModel;
+
+namespace Welcome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("Ventsy", "ventsy123", Others.UserRolesEnum.STUDENT, "121221095", "ventsy@gmail.com");
+            UserViewModel viewModel = new UserViewModel(user);
+            UserView view = new UserView(viewModel);
+            view.Display();
+            Console.ReadKey();
+
+        }
+
+
+
+
+    }
+}
